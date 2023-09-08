1634c8ccc1c1c62a986a40d460edddaedacbb0b7
FabrizioGrosso16 <142623045+FabrizioGrosso16@users.noreply.github.com>
2023-09-08 13:56:22 -0500

    Add files via upload

---
+++ src/casot1/Personita.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package casot1;
+
+/**
+ *
+ * @author User001
+ */
+public class Personita {
+    String nombre;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public Personita(String nombre)
+    {
+        this.nombre =nombre;
+    }
+    
+}
